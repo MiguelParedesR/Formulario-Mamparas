73d8323d2b938af558a1bb0b056333ef612d8f1b
MiguelParedesR <miguelparedesramirez14@gmail.com>
2026-09-25 15:01:19 -0500

    chore(redesign): refresh modern UI assets

--- service-worker.js
+++ service-worker.js
@@ -13,11 +13,12 @@ const asset = (path = "") => {
   return BASE_PATH ? `${BASE_PATH}/${cleaned}` : cleaned;
 };
 
-const VERSION = "v7.139";
+const VERSION = "v7.140";
 const CACHE_NAME = `CCTV-${VERSION}${BASE_PATH ? `-${BASE_PATH.replace(/\//g, "-")}` : ""}`;
 
 const STATIC_ASSETS = [
   "CSS/global.css",
+  "CSS/apple-system.css",
   "CSS/tailwind.css",
   "CSS/dashboard/dashboard.css",
   "CSS/estilos-sidebar/sidebar.css",
@@ -27,6 +28,11 @@ const STATIC_ASSETS = [
   "js/mamparas/reportes.js",
   "js/libs/docxtemplater-image-module.js",
   "js/dashboard/dashboard.js",
+  "html/formulario.html",
+  "html/registros.html",
+  "html/formulario-mamparas/registro.html",
+  "html/formulario-mamparas/registros.html",
+  "html/formulario-mamparas/reportes.html",
   "manifest.json",
   "favicon.ico",
 ].map(asset);
